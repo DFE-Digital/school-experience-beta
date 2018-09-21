61e0ba35e33a21d2ba88bd85e62384dc74f94cee
Neil Scales <neil.scales@outlook.com>
2018-09-21 14:53:37 +0100

    Created new project.

---
+++ src/SchoolExperienceSchoolUi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolExperienceSchoolUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
